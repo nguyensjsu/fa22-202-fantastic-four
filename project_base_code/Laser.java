57738b6f33b10986632a4a110cc312bf1a6ccf06
Ray Sheng <shengyumeng@gmail.com>
2022-11-05 12:48:21 -0700

    fixed issues created from merge conflicts

--- project_base_code/Laser.java
+++ project_base_code/Laser.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import greenfoot.*;  
 public class Laser extends Actor
 {
@@ -15,22 +14,4 @@ public class Laser extends Actor
             getWorld().removeObject(this);
         }
     }         
-=======
-import greenfoot.*;  
-public class Laser extends Actor
-{
-    public Laser(){
-        getImage().scale(10,20);
-    }
-    public void act() 
-    {
-        setLocation(getX(),getY()-20);
-        behavior();
-    }    
-    public void behavior(){
-        if(getY()==0){
-            getWorld().removeObject(this);
-        }
-    }         
->>>>>>> 1361153b5f101c364e745cd82527e23f2b9dcddd
 }
