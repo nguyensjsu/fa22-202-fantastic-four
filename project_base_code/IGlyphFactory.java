0ea39edb35f615a01ac5d1fbb18100cb8b88d627
motwave <archer.yiyang@gmail.com>
2022-12-03 01:17:56 -0800

    updated interface for accessing glyph factories

---
+++ project_base_code/IGlyphFactory.java
@@ -0,0 +1,11 @@
+/**
+ * Write a description of class IGlyphFactory here.
+ * 
+ * @author (Jing) 
+ * @version (v1.0)
+ */
+public interface IGlyphFactory  
+{
+    void CreateButton();
+    void CreateLabel();
+}
