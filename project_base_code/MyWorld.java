289126ee67c69621d06fae2e1c800677755993ff
motwave <archer.yiyang@gmail.com>
2022-12-03 02:14:13 -0800

    updated world methods to support more functions.

--- project_base_code/MyWorld.java
+++ project_base_code/MyWorld.java
@@ -4,10 +4,38 @@ public class MyWorld extends World
     public String[] images = {"blue.png","orange.png","pink.png","purple.png","teal.png","tan.png","white.png","yeller.png","red.png"};
     public int[] xcord = {74,177,288,27,132,479,144,408,19,80,264,380,406,491,18,85,165,206,296,106,49,25,13,78,89,145,138,167,221,234,245,371,449,347,299,379,440,291,462,393,282,338,448,318,398,456};
     public int[] ycord = {11,26,41,58,54,71,99,83,121,152,124,113,129,152,214,227,187,206,191,262,276,343,472,407,447,336,458,416,470,334,270,182,185,227,294,276,302,379,356,402,438,417,427,481,475,480};
+    
+    private Screen currentScreen;
+    private MenuScreen menuScreen;
+    private GameScreen gameScreen;
+    private LocalScoreBoard scoreBoard;
+    private GameOverScreen gameOverScreen;
+    private LeaderboardScreen leaderboardScreen;
+    
+    private MenuScreenGlyphFactory menuScreenGlyphFactory;
+    private LeaderboardScreenGlyphFactory lbSGlyphFactory;
+    private GameOverScreenGlyphFactory gOSGlyphFactory;
+    
     public MyWorld()
     {    
-        super(500, 500, 1);
-        makeActors();
+        super(500, 500, 1);  
+        //reset();
+        // Create GlyphFactory for screens
+        menuScreenGlyphFactory = new MenuScreenGlyphFactory(this);
+        lbSGlyphFactory = LeaderboardScreenGlyphFactory.getInstance(this);
+        gOSGlyphFactory = new GameOverScreenGlyphFactory(this);
+        // Create screens in MyWorld
+        menuScreen = new MenuScreen(this);
+        //gameScreen = new GameScreen(this);
+        //gameOverScreen = new GameOverScreen(this);
+        //leaderboardScreen = new LeaderboardScreen(this);
+        
+        //scoreBoard = LocalScoreBoard.getInstance(400,300,this);
+        //scoreBoard.drawNewScore("Your new score is: " + String.valueOf(this.getCounter().getValue()), 200, 20, new Color(0x0, 0x0, 0x0), 25);
+        //scoreBoard.drawScores();
+        
+        setNextScreen(SCREENS.MENU);
+        //makeActors();
     }
     
     public enum SCREENS 
@@ -43,5 +71,57 @@ public class MyWorld extends World
         addObject(stage,43,11);
     }
     
+    public LocalScoreBoard getScoreBoard(){
+        return scoreBoard;
+    }
     
+    /**
+     * A method to set nextScreen and activate the Screen
+     * 
+     */
+    public void setNextScreen(SCREENS screen)
+    {
+        if(currentScreen !=null)
+            currentScreen.clean();
+            
+        switch(screen) {
+            case MENU: {
+                currentScreen = menuScreen; 
+                break;
+            }
+            case GAME: {
+                GameScreen.initialize();
+                currentScreen = gameScreen; 
+                break;
+            }
+            case LEADERBOARD: {
+                currentScreen = leaderboardScreen; 
+                break;
+            }
+            default: currentScreen = menuScreen;
+        }
+        currentScreen.active();
+        
+    }
+    
+    public void setGameScreen() {
+        currentScreen = gameScreen;
+        currentScreen.active();
+    }
+    
+    public void stop() {
+        currentScreen = gameOverScreen;
+        currentScreen.active();
+    }
+    /**
+     * A "Act" method in the world activate the currentScreen
+     * 
+     */    
+    @Override
+    public void act()
+    {
+        if(currentScreen != null) {
+            currentScreen.active();
+        }
+    }
 }
